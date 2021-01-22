c62a3b18ab073cece35e716d281c1de86d8a8004
ashok <ashokakpost@gmail.com>
2021-01-22 18:07:37 +0530

    Added new pdf file url

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -1,6 +1,6 @@
 let CONFIG = {}
 
 CONFIG.FILEONE =
-  "https://firebasestorage.googleapis.com/v0/b/studied-theater-295812.appspot.com/o/sample%20Q%26A.pdf?alt=media&token=d3773376-3e97-44f8-9c3f-631978881ae9";
+  "https://firebasestorage.googleapis.com/v0/b/studied-theater-295812.appspot.com/o/sample2.pdf?alt=media&token=e1605aad-4d4d-4ea7-a9b6-7b17fe98022e";
 
 export default CONFIG;
